12a0ddb24709b8abec0ac64051acb55ebfc772b7
Erin Varey <vareye@mcmaster.ca>
2018-11-26 20:57:43 -0500

    last of changes

--- src/views/login/fake-card.vue
+++ src/views/login/fake-card.vue
@@ -14,21 +14,37 @@
     </div>
     <div class="row text-center">
       <div class="col-sm">
-        <img src="assets/card.jpeg"/>
-      </div>
+        <img id="cardImg" src="assets/card.jpeg"/>
+      </div> 
     </div>
+    <br/>
+    <div align="center">
+      <h2 class="sub-title">Input Account Number</h2>
+      <input type="text" maxlength="4" style="width: 50px;"/>-<input type="text" maxlength="4" style="width: 50px;"/>-<input type="text" maxlength="4" style="width: 50px;"/>
+      <br/>
+      <br/>
+        <div class="buttonBox">
+          <button v-on:click="submit" class="btn btn-success physical-btn checkmark">&#10003</button>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
 module.exports = {
+  methods: {
+    submit () {
+      this.$router.push({path: '/pin'}) 
+    }
+  },
   mounted () {
     const self = this
     function route () {
       window.removeEventListener('click', route)
       self.$router.push('/pin') 
     }
-    window.addEventListener('click', route)
+    cardImg = document.getElementById("cardImg");
+    cardImg.addEventListener('click', route)
   }
 }
 </script>
